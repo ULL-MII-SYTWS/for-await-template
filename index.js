a1a455037a1bdbd8659ad293138cd1abb439419a
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-11-07 07:53:38 +0000

    async-iteration-and-generators

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // promises is an Array not an Iterable
-async function* auxIterator(promises) {
-  // Write your code here
-}
-
 async function * frstcmfrstsvd(promises) {
   // Write your code here
 }
